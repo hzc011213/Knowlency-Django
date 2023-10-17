f944dffb1d8c85af01b5ebca8a35f48ff9019c10
hzc011213 <hzc011213@gmail.ccom>
2023-10-16 21:25:17 -0400

    update

--- Knowlency/settings.py
+++ Knowlency/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e#da)@(3@7+&ni9ewz*9isy)6e-9rcnyj67w(6t+s9d609^*)f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
